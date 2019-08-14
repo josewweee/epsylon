a2425b44f84125a933012e5049d9be56a310d2d7
josewweee <josemariamira@hotmail.com>
2019-08-14 13:23:55 -0500

    agregando niveles
    Agregando escena de charecter selection, historia, nivel 1 y nivel 2, los botones estan conectados.

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
-    public void GotoMainScene()
+    public void GotoCharacterSelection()
     {
-        SceneManager.LoadScene("main");
+        SceneManager.LoadScene("character_selection");
     }
 
+   
+    public void GotoStoryScene()
+    {
+        SceneManager.LoadScene("story_time");
+    }
+
+    public void GotoLevel_1()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void GotoLevel_2()
+    {
+        SceneManager.LoadScene("level2");
+    }
 }
