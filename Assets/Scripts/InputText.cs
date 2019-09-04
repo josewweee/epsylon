f17180fc690caa28c526c71e63834978b6c5ef34
Mortallica <41587633+Mortallica@users.noreply.github.com>
2019-09-04 10:27:18 -0500

    Unificando el proyecto
    Se agrego el nivel 1 con las modificaciones

---
+++ Assets/Scripts/InputText.cs
@@ -0,0 +1,33 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.UI;
+ using System;
+ 
+ public class InputText : MonoBehaviour
+ {
+ 
+     InputField input;
+     InputField.SubmitEvent se;
+     public Text output;
+ 
+ 
+     void Start()
+     {
+ 
+         input = gameObject.GetComponent<InputField>();
+         se = new InputField.SubmitEvent();
+         se.AddListener(SubmitInput);
+         input.onEndEdit = se;
+ 
+     }
+ 
+     private void SubmitInput(string A)
+     {
+         string currentText = output.text;
+         string newText = currentText + "\n" + ">" + A;
+         output.text = newText;
+         input.text = "";
+         input.ActivateInputField();
+     }
+ }
+ 
