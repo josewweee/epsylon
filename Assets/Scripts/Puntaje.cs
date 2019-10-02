c70df16cf5c7d70d4ef303059d995d41ba598f04
josewweee <josemb125@gmail.com>
2019-10-02 10:22:57 -0500

    modificando bugs audio greenOnion, Agregando UI de puntaje en niveles historia, quiz y 3

--- Assets/Scripts/Puntaje.cs
+++ Assets/Scripts/Puntaje.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puntaje : MonoBehaviour
 {
     public static float puntajeJugador = 0;
+    private static float maximoPuntaje = puntajeJugador;
+    public Text puntaje_UI;
+
 
     void Start()
     {
+        
+        Debug.Log(puntajeJugador);
         DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( puntajeJugador > maximoPuntaje ) {
+            puntaje_UI = GameObject.FindWithTag("Slot").GetComponent<Text>();
+            puntaje_UI.text = puntajeJugador.ToString();
+        }
     }
 }
